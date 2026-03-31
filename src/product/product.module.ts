6ab79e6dd7026114363e9bb3213fe67533c5e7be
Mohamed Hakeem <m@gmail.com>
2026-03-31 20:52:17 +0200

    update productModule

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,10 +3,16 @@ import { productService } from './product.service';
 import { productController } from './product.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { productSchema } from './product.schema';
+import { categorySchema } from 'src/category/category.schema';
+import { subCategorySchema } from 'src/sub-category/sub-category.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Product', schema: productSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Product', schema: productSchema },
+      { name: 'Category', schema: categorySchema },
+      { name: 'SubCategory', schema: subCategorySchema },
+    ]),
   ],
   controllers: [productController],
   providers: [productService],
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -5,22 +5,28 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './product.schema';
 import { Model } from 'mongoose';
 import { APIFeatures } from 'src/utils/apiFeatures';
+import { Category } from 'src/category/category.schema';
+import { SubCategory } from 'src/sub-category/sub-category.schema';
 
 @Injectable()
 export class productService {
-  constructor(@InjectModel('Product') private ProductModel: Model<Product>) {}
+  constructor(
+    @InjectModel('Product') private ProductModel: Model<Product>,
+    @InjectModel('Category') private categoryModel: Model<Category>,
+    @InjectModel('SubCategory') private subCategoryModel: Model<SubCategory>,
+  ) {}
   async create(createProductDto: CreateProductDto) {
     const product = await this.ProductModel.findOne({
       name: createProductDto.title,
     });
     if (product) throw new HttpException('Product already exist', 400);
 
-    const category = await this.ProductModel.findById(
+    const category = await this.categoryModel.findById(
       createProductDto.category,
     );
     if (!category) throw new HttpException('category not exist', 400);
 
-    const subCategory = await this.ProductModel.findById(
+    const subCategory = await this.subCategoryModel.findById(
       createProductDto.subCategory,
     );
     if (!subCategory) throw new HttpException('subCategory not exist', 400);
